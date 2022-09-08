6549f4eea399267cd33d00738f23fe38ff4d58d0
NishanHolla <92212948+NishanHolla@users.noreply.github.com>
2022-09-08 12:11:06 +0530

    Add files via upload

---
+++ Calculator/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Calculator from "./components/body";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Calculator/>)    
